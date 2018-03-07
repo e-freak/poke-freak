14e09ac6d4a6f352f42540bf323d85658c288a1e
clavicula <u.n@owen>
2018-03-07 09:43:49 +0900

    サンプルパーティーを更新

---
+++ src/stub/js/pokemon/automaton/party-resource-list.js
@@ -0,0 +1,188 @@
+/**
+ * party-resource-list.js
+ * 
+ * @author yuki
+ */
+
+
+
+export default [
+    
+    {
+        'カバルドン': {
+            pokemonID: 4500,
+            nature: 'IMPISH',
+            ability: '砂起こし',
+            skillNameList: [ '地震', '岩石封じ', '炎の牙', '氷の牙' ],
+            item: 'オボンの実',
+            effortValue: { H: 252, A: 252, B: 4, C: 0, D: 0, S: 0 },
+        },
+        
+        'ドリュウズ': {
+            pokemonID: 5300,
+            nature: 'ADAMANT',
+            ability: '砂掻き',
+            skillNameList: [ '地震', 'アイアンヘッド', '岩なだれ', '剣の舞' ],
+            item: 'ヨプの実',
+            effortValue: { H: 0, A: 252, B: 4, C: 0, D: 0, S: 252 },
+        },
+        
+        'ラティオス': {
+            pokemonID: 3810,
+            nature: 'TIMID',
+            ability: '浮遊',
+            skillNameList: [ '流星群', 'サイコキネシス', '波乗り', '目覚めるパワー' ],
+            item: '白いハーブ',
+            effortValue: { H: 4, A: 0, B: 0, C: 252, D: 0, S: 252 },
+            hiddenElement: 'FIRE',
+        },
+        
+        'ウルガモス': {
+            pokemonID: 6370,
+            nature: 'MODEST',
+            ability: '炎の体',
+            skillNameList: [ '火炎放射', '虫のさざめき', '目覚めるパワー', '蝶の舞' ],
+            item: 'ラムの実',
+            effortValue: { H: 4, A: 0, B: 0, C: 252, D: 0, S: 252 },
+            hiddenElement: 'GROUND',
+        },
+        
+        'パルシェン': {
+            pokemonID: 910,
+            nature: 'ADAMANT',
+            ability: 'スキルリンク',
+            skillNameList: [ '氷柱落とし', 'ハイドロポンプ', '氷の礫', '殻を破る' ],
+            item: '命の珠',
+            effortValue: { H: 0, A: 252, B: 4, C: 0, D: 0, S: 252 },
+        },
+        
+        'キノガッサ': {
+            pokemonID: 2860,
+            nature: 'ADAMANT',
+            ability: 'テクニシャン',
+            skillNameList: [ 'タネ爆弾', 'マッハパンチ', '岩石封じ', '剣の舞' ],
+            item: '気合のタスキ',
+            effortValue: { H: 0, A: 252, B: 4, C: 0, D: 0, S: 252 },
+        },
+        
+    },
+    
+    {
+        '化身ボルトロス': {
+            pokemonID: 6420,
+            nature: 'TIMID',
+            ability: '悪戯心',
+            skillNameList: [ '10万ボルト', '目覚めるパワー', '悪巧み', '電磁波' ],
+            item: 'オボンの実',
+            effortValue: { H:252, A: 0, B: 0, C: 4, D: 0, S: 252 },
+            hiddenElement: 'ICE',
+        },
+        
+        'ガブリアス': {
+            pokemonID: 4450,
+            nature: 'JOLLY',
+            ability: '鮫肌',
+            skillNameList: [ '地震', '逆鱗', '岩石封じ', '剣の舞' ],
+            item: '気合のタスキ',
+            effortValue: { H: 0, A: 252, B: 4, C: 0, D: 0, S: 252 },
+        },
+        
+        'カイリュー': {
+            pokemonID: 1490,
+            nature: 'ADAMANT',
+            ability: 'マルチスケイル',
+            skillNameList: [ '逆鱗', '炎のパンチ', '神速', '竜の舞' ],
+            item: 'ラムの実',
+            effortValue: { H: 4, A: 252, B: 0, C: 0, D: 0, S: 252 },
+        },
+        
+        'ローブシン': {
+            pokemonID: 5340,
+            nature: 'ADAMANT',
+            ability: '力ずく',
+            skillNameList: [ 'ドレインパンチ', '冷凍パンチ', '雷パンチ', 'マッハパンチ' ],
+            item: '命の珠',
+            effortValue: { H: 4, A: 252, B: 0, C: 0, D: 0, S: 252 },
+        },
+        
+        'クレセリア': {
+            pokemonID: 4880,
+            nature: 'MODEST',
+            ability: '浮遊',
+            skillNameList: [ 'サイコキネシス', 'ムーンフォース', '冷凍ビーム', '目覚めるパワー' ],
+            item: 'ゴツゴツメット',
+            effortValue: { H: 252, A: 0, B: 4, C: 252, D: 4, S: 0 },
+            hiddenElement: 'FIRE',
+        },
+        
+        'スイクン': {
+            pokemonID: 2450,
+            nature: 'BOLD',
+            ability: 'プレッシャー',
+            skillNameList: [ '熱湯', '冷凍ビーム', '目覚めるパワー', '瞑想' ],
+            item: '食べ残し',
+            effortValue: { H: 252, A: 0, B: 252, C: 0, D: 4, S: 0 },
+            hiddenElement: 'FIRE',
+        },
+    },
+    
+    {
+        '化身ボルトロス': {
+            pokemonID: 6420,
+            nature: 'TIMID',
+            ability: '悪戯心',
+            skillNameList: [ '10万ボルト', '目覚めるパワー', '悪巧み', '電磁波' ],
+            item: 'オボンの実',
+            effortValue: { H:252, A: 0, B: 0, C: 4, D: 0, S: 252 },
+            hiddenElement: 'ICE',
+        },
+        
+        'キノガッサ': {
+            pokemonID: 2860,
+            nature: 'ADAMANT',
+            ability: 'テクニシャン',
+            skillNameList: [ 'タネ爆弾', 'マッハパンチ', '岩石封じ', '剣の舞' ],
+            item: '気合のタスキ',
+            effortValue: { H: 0, A: 252, B: 4, C: 0, D: 0, S: 252 },
+        },
+        
+        'スイクン': {
+            pokemonID: 2450,
+            nature: 'BOLD',
+            ability: 'プレッシャー',
+            skillNameList: [ '熱湯', '冷凍ビーム', '目覚めるパワー', '瞑想' ],
+            item: '食べ残し',
+            effortValue: { H: 252, A: 0, B: 252, C: 0, D: 4, S: 0 },
+            hiddenElement: 'FIRE',
+        },
+        
+        'ラティオス': {
+            pokemonID: 3810,
+            nature: 'TIMID',
+            ability: '浮遊',
+            skillNameList: [ '流星群', 'サイコキネシス', '波乗り', '目覚めるパワー' ],
+            item: '白いハーブ',
+            effortValue: { H: 4, A: 0, B: 0, C: 252, D: 0, S: 252 },
+            hiddenElement: 'FIRE',
+        },
+        
+        'カイリュー': {
+            pokemonID: 1490,
+            nature: 'ADAMANT',
+            ability: 'マルチスケイル',
+            skillNameList: [ '逆鱗', '炎のパンチ', '神速', '竜の舞' ],
+            item: 'ラムの実',
+            effortValue: { H: 4, A: 252, B: 0, C: 0, D: 0, S: 252 },
+        },
+        
+        'ハッサム': {
+            pokemonID: 2120,
+            nature: 'ADAMANT',
+            ability: 'テクニシャン',
+            skillNameList: [ 'バレットパンチ', '虫食い', '電光石火', '剣の舞' ],
+            item: '命の珠',
+            effortValue: { H: 236, A: 252, B: 20, C: 0, D: 0, S: 0 },
+        },
+    },
+    
+];
